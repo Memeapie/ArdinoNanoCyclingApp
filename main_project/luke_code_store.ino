ae5cd7c75a3974ea16ab4c0f4edc7c88d608d9f4
lw8256n <luke@threluja.co.uk>
2021-04-06 20:23:56 +0100

    Inserted code which reads pressure from the sensor

--- main_project/luke_code_store.ino
+++ main_project/luke_code_store.ino
@@ -1,2 +1,30 @@
 //place for luke to store code to show progress, will move into main_project.ino when complete
 
+
+#include <Arduino_LPS22HB.h>
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  if (!BARO.begin()) {
+    Serial.println("Failed to initialize pressure sensor!");
+    while (1);
+  }
+}
+
+void loop() {
+  // read the sensor value
+  float pressure = BARO.readPressure();
+
+  // print the sensor value
+  Serial.print("Pressure = ");
+  Serial.print(pressure);
+  Serial.println(" kPa");
+
+  // print an empty line
+  Serial.println();
+
+  // wait 1 second to print again
+  delay(1000);
+}
