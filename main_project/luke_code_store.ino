72280923af0749bb72433803b3398c5bd4a262a8
lw8256n <luke@threluja.co.uk>
2021-04-06 21:13:24 +0100

    Tried to calculate distance from sea level using pressure sensor, need to tidy up code

--- main_project/luke_code_store.ino
+++ main_project/luke_code_store.ino
@@ -2,6 +2,12 @@
 
 
 #include <Arduino_LPS22HB.h>
+#include <math.h> //library for maths
+
+
+
+double Po = 1013.0; //atmpshpehric pressure at sea level, need to get a value for kent coast
+//double pow;
 
 void setup() {
   Serial.begin(9600);
@@ -21,6 +27,33 @@ void loop() {
   Serial.print("Pressure = ");
   Serial.print(pressure);
   Serial.println(" kPa");
+
+
+  double pressureHPA = pressure * 10;
+  Serial.print("Pressure = ");
+  Serial.print(pressureHPA);
+  Serial.println(" hPa");
+
+
+  double test = 1/5.255;
+  double test2 = pressureHPA / Po;
+  double test3 = pow(test2, test);
+  double test4 = 1 - test3;
+  double test5 = 44340 * test4; // this outputs a result in hectopascal
+
+  Serial.print(test); //obviously will tidy this up when i've got it working properly, currently says i'm -36m below seaLevel
+  Serial.println();
+  Serial.print(test2);
+  Serial.println();
+  Serial.print(test3);
+  Serial.println();
+  Serial.print(test4);
+  Serial.println();
+  Serial.print(test5);
+
+  //pow = ((pressure/Po), test);
+  //Serial.print(pow);
+  //float altitude;
 
   // print an empty line
   Serial.println();
