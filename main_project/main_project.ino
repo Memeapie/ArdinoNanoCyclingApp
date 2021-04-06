a666fc39f3ad24a74b5a61ce797bdb413b754e92
Memeapie <samuel47@virginmedia.com>
2021-04-06 15:25:20 +0100

    Cleaned Out Main Project Ready for Team Code

--- main_project/main_project.ino
+++ main_project/main_project.ino
@@ -3,23 +3,6 @@
 #include <PDM.h>
 #include <Arduino_LSM9DS1.h>
 
-// buffer to read samples into, each sample is 16-bits
-short sampleBuffer[256];
-
-// number of samples read
-volatile int samplesRead;
-
-void onPDMdata() {
-    // query the number of bytes available
-    int bytesAvailable = PDM.available();
-
-    // read into the sample buffer
-    int bytesRead = PDM.read(sampleBuffer, bytesAvailable);
-
-    // 16-bit, 2 bytes per sample
-    samplesRead = bytesRead / 2;
-}
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -45,38 +28,5 @@ void setup() {
 }
 
 void loop() {
-  /*float temp = HTS.readTemperature();
-  float humid = HTS.readHumidity();
 
-  Serial.println(temp);
-  Serial.println(humid);
-  */
-
-  /*
-  if(samplesRead){
-
-      for(int i = 0; i < sampleBuffer[i]; i++){
-        if(sampleBuffer[i] > -40 && sampleBuffer[i] < 40){
-           sampleBuffer[i] = 0;
-        }
-        Serial.println(sampleBuffer[i]);
-      }
-
-      samplesRead = 0;
-  }
-  */
-
-  float x, y, z;
-
-  if (IMU.gyroscopeAvailable()) {
-        IMU.readGyroscope(x, y, z);
-
-        Serial.print(x);
-        Serial.print('\t');
-        Serial.print(y);
-        Serial.print('\t');
-        Serial.println(z);
-  }
-
-  delay(250);
 }
