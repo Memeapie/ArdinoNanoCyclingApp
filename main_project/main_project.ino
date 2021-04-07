039a453aa16d9ed20f1fe00cfe7af675f7eedecc
Memeapie <samuel47@virginmedia.com>
2021-04-07 16:57:03 +0100

    Cleaned Mike's Code

--- main_project/main_project.ino
+++ main_project/main_project.ino
@@ -3,6 +3,20 @@
 #include <PDM.h>
 #include <Arduino_LSM9DS1.h>
 
+// Global Variables
+// Temperature & Humidity Values
+float temp;
+float tempH;
+float humidity;
+float humidityH
+
+// Temperature & Humidity Task Control
+int tempHumidityInterval = 500; // The task interval in ms
+int tempHumidityIntervalH; // How long it took last time.
+int tempHumidityLength; // Length of task this time.
+int tempHumidityStartTime; // The start time of the task in ms.
+int tempHumidityStartTimeH = millis(); // The clock when the task starts history.
+
 void setup() {
     // Initialise Serial Output
     Serial.begin(9600);
@@ -36,6 +50,10 @@ void setup() {
 
 void loop() {
 
+    float humid = HTS.readHumidity();
+
+    Serial.println(temp);
+    Serial.println(humid);
 
 
 }
--- mike_example_code/mike_example_code.ino
+++ mike_example_code/mike_example_code.ino
@@ -73,52 +73,48 @@ BLECharCharacteristic compassHeading("2107", BLERead | BLENotify);
 
 
 void setup(){
-Serial.begin(9600); //Start the serial port.
-while (!Serial); //Wait for serial to start.
-if (!HTS.begin()) {
-Serial.println("Failed to initialize humidity temperature sensor!");
-while (1);
-}
-PDM.onReceive(audioCallBack);
+    Serial.begin(9600); //Start the serial port.
+    while (!Serial); //Wait for serial to start.
 
-if (!PDM.begin(1, 16000)) {
-Serial.println("Failed to start PDM!");
-while (1);
-}
+    if (!HTS.begin()) {
+        Serial.println("Failed to initialize humidity temperature sensor!");
+        while (1);
+    }
+    PDM.onReceive(audioCallBack);
 
+    if (!PDM.begin(1, 16000)) {
+        Serial.println("Failed to start PDM!");
+        while (1);
+    }
 
+    if (!IMU.begin()) {
+        Serial.println("Failed to initialize IMU!");
+        while (1);
+    }
 
-if (!IMU.begin()) {
-Serial.println("Failed to initialize IMU!");
-while (1);
-}
-if (!BLE.begin())
-{
-Serial.println("starting BLE failed!");
-while (1);
-}
-BLE.setLocalName("MultiTasker");
-BLE.setAdvertisedService(testService);
-//Now for everything we want to send over our BLE service we need to set the characteristics up
-//First those we want for Task 1 the accelerometer.
-testService.addCharacteristic(accelLevelX);
-testService.addCharacteristic(accelLevelY);
-testService.addCharacteristic(accelLevelZ);
-//Now for compass
-testService.addCharacteristic(compassLevelX);
-testService.addCharacteristic(compassLevelY);
-testService.addCharacteristic(compassLevelZ);
-testService.addCharacteristic(compassHeading);
+    if (!BLE.begin()) {
+        Serial.println("starting BLE failed!");
+        while (1);
+    }
 
-BLE.addService(testService);
+    BLE.setLocalName("MultiTasker");
+    BLE.setAdvertisedService(testService);
+    //Now for everything we want to send over our BLE service we need to set the characteristics up
+    //First those we want for Task 1 the accelerometer.
+    testService.addCharacteristic(accelLevelX);
+    testService.addCharacteristic(accelLevelY);
+    testService.addCharacteristic(accelLevelZ);
+    //Now for compass
+    testService.addCharacteristic(compassLevelX);
+    testService.addCharacteristic(compassLevelY);
+    testService.addCharacteristic(compassLevelZ);
+    testService.addCharacteristic(compassHeading);
 
+    BLE.addService(testService);
 
-
-//Forgot this pair of lines
-BLE.advertise();
-Serial.println("Bluetooth device active, waiting for connections...");
-
-
+    //Forgot this pair of lines
+    BLE.advertise();
+    Serial.println("Bluetooth device active, waiting for connections...");
 
 }
 
@@ -128,167 +124,163 @@ void loop(){
 
 
 
-float temperature = HTS.readTemperature();
-//Serial.print("Temperature = ");
-//Serial.print(temperature);
-//Serial.println(" °C");
-// wait for samples to be read
-if (samplesRead) {
+    float temperature = HTS.readTemperature();
+    //Serial.print("Temperature = ");
+    //Serial.print(temperature);
+    //Serial.println(" °C");
+    // wait for samples to be read
+    if (samplesRead) {
 
 
 
-// print samples to the serial monitor or plotter
-for (int i = 0; i < samplesRead; i++) {
-if(sampleBuffer[i] > -40 && sampleBuffer[i] < 40){
-sampleBuffer[i] = 0;
-}
-//Serial.println(sampleBuffer[i]);
-}
+        // print samples to the serial monitor or plotter
+        for (int i = 0; i < samplesRead; i++) {
+            if(sampleBuffer[i] > -40 && sampleBuffer[i] < 40){
+                sampleBuffer[i] = 0;
+            }
+            //Serial.println(sampleBuffer[i]);
+        }
 
 
 
-// clear the read count
-samplesRead = 0;
+        // clear the read count
+        samplesRead = 0;
 
-}
-
-
-
-//********************************** The TASK 1 management *****************************
-//Now lets set up our task processings system. There are many ways we could do this
-//but for now we will keep it simple. Each task will be handled in the same way.
-//First some preliminaries for the task - we will get set up
-task1_StartTime = millis(); //First grab the time now, we will need this as a fixed point
-//so getting it now is valuable and freezes it for us.
-if(task1_StartTime > (task1_StartTimeH+task1_Interval)){ // We want to do task1 otherwise not.
-//First lets check that things have not gone very badly.
-if(task1_StartTime > (task1_StartTimeH+task1_FailInterval)){ //We have failed and we must report
-if(testing){
-Serial.println("Task 1 failed to happen in time.");
-delay(300);
-}
-task1_Error = ERRORTIMING;
-task1_StartTimeH = task1_StartTime;
-}
-else //We can carry on to do our task.
-{
-//Put all our task stuff here.
-//In ths case just grab the magnatometor data.
+    }
 
 
 
-if (IMU.magneticFieldAvailable()) {
-IMU.readMagneticField(cx, cy, cz);
-//Now we can find out how long our task has taken and update our histories.
+    //********************************** The TASK 1 management *****************************
+    //Now lets set up our task processings system. There are many ways we could do this
+    //but for now we will keep it simple. Each task will be handled in the same way.
+    //First some preliminaries for the task - we will get set up
+    task1_StartTime = millis(); //First grab the time now, we will need this as a fixed point
+    //so getting it now is valuable and freezes it for us.
+    if(task1_StartTime > (task1_StartTimeH+task1_Interval)){ // We want to do task1 otherwise not.
+        //First lets check that things have not gone very badly.
+        if(task1_StartTime > (task1_StartTimeH+task1_FailInterval)){ //We have failed and we must report
+            if(testing){
+                Serial.println("Task 1 failed to happen in time.");
+                delay(300);
+            }
+            task1_Error = ERRORTIMING;
+            task1_StartTimeH = task1_StartTime;
+            } else {
+                //Put all our task stuff here.
+                //In ths case just grab the magnatometor data.
 
 
 
-//Now work out heading
-float h = (atan2(cy,cx)*180)/PI;
-//Copy calced value to variable for BLE handling.
-compassHeadingValue = h;
-
-if(testing){
-Serial.print(cx);
-Serial.print('\t');
-Serial.print(cy);
-Serial.print('\t');
-Serial.print(cz);
-Serial.print('\t');
-Serial.println(h);
-}
-}
-else {
-//The magnatometer did not start - won't hold up but will report.
-if(task1_Error == ERRORCLEAR){
-if(testing){
-Serial.println(" Magnatometer did not start.");
-}
-task1_Error = ERRORSENSOR;
-}
-}
-//Now lets add a delay to check our timing system - we can remove this later.
-//delay(50);
-//Now we can find out how long our task has taken and update our histories.
-task1_Length = millis() - task1_StartTime;
-task1_StartTimeH = task1_StartTime; //Copy history so we can use it to trigger the next shot
-if(testing){
-Serial.print("Task 1 time interval: ");
-Serial.print(task1_Length);
-Serial.println(" Milliseconds");
-}
-}
-}
+                if (IMU.magneticFieldAvailable()) {
+                    IMU.readMagneticField(cx, cy, cz);
+                    //Now we can find out how long our task has taken and update our histories.
 
 
 
-BLE_StartTime = millis();
-if(BLE_StartTime > (BLE_StartTimeH+BLE_interval)) {
-BLE_servicing();
-}
+                    //Now work out heading
+                    float h = (atan2(cy,cx)*180)/PI;
+                    //Copy calced value to variable for BLE handling.
+                    compassHeadingValue = h;
+
+                    if(testing){
+                        Serial.print(cx);
+                        Serial.print('\t');
+                        Serial.print(cy);
+                        Serial.print('\t');
+                        Serial.print(cz);
+                        Serial.print('\t');
+                        Serial.println(h);
+                    }
+                } else {
+                    //The magnatometer did not start - won't hold up but will report.
+                    if(task1_Error == ERRORCLEAR){
+                        if(testing){
+                            Serial.println(" Magnatometer did not start.");
+                        }
+                        task1_Error = ERRORSENSOR;
+                    }
+                }
+                //Now lets add a delay to check our timing system - we can remove this later.
+                //delay(50);
+                //Now we can find out how long our task has taken and update our histories.
+                task1_Length = millis() - task1_StartTime;
+                task1_StartTimeH = task1_StartTime; //Copy history so we can use it to trigger the next shot
+                if(testing){
+                    Serial.print("Task 1 time interval: ");
+                    Serial.print(task1_Length);
+                    Serial.println(" Milliseconds");
+                }
+            }
+        }
 
 
 
-// Now longer used delay(1000);
-if(testing){
-Serial.println("Loop");
-delay(500);
-}
-}
+        BLE_StartTime = millis();
+        if(BLE_StartTime > (BLE_StartTimeH+BLE_interval)) {
+            BLE_servicing();
+        }
 
 
 
-void BLE_servicing(){
-BLEDevice central = BLE.central();
+        // Now longer used delay(1000);
+        if(testing){
+            Serial.println("Loop");
+            delay(500);
+        }
+    }
 
-if (central)
-{
-if(testing) {
-Serial.print("Connected to central: ");
-Serial.println(central.address());
-}
-digitalWrite(LED_BUILTIN, HIGH);
 
-if (central.connected()) { // We can do our sending to the BLE system
-if(testing) {
-Serial.print("AccelX % is now: ");
-Serial.println(x);
-}
-//Now task by task send out the values.
-//Remember these may need timed handling as well so task by task
-//depending on timings needed for the client device the other end. (Watch / phone /...)
-accelLevelX.writeValue(x);
-accelLevelY.writeValue(y);
-accelLevelZ.writeValue(z);
-compassLevelX.writeValue(cx);
-compassLevelY.writeValue(cy);
-compassLevelZ.writeValue(cz);
-compassHeading.writeValue(compassHeadingValue);
-}
-}
-else {
-Serial.print("Central disconnected: ");
-Serial.println(" - ");
-digitalWrite(LED_BUILTIN, LOW);
-}
-//Now lets get the history set up.
-BLE_StartTimeH = BLE_StartTime;
+
+    void BLE_servicing(){
+    BLEDevice central = BLE.central();
+
+    if (central) {
+        if (testing) {
+            Serial.print("Connected to central: ");
+            Serial.println(central.address());
+        }
+        digitalWrite(LED_BUILTIN, HIGH);
+
+        if (central.connected()) { // We can do our sending to the BLE system
+            if(testing) {
+                Serial.print("AccelX % is now: ");
+                Serial.println(x);
+            }
+            //Now task by task send out the values.
+            //Remember these may need timed handling as well so task by task
+            //depending on timings needed for the client device the other end. (Watch / phone /...)
+            accelLevelX.writeValue(x);
+            accelLevelY.writeValue(y);
+            accelLevelZ.writeValue(z);
+            compassLevelX.writeValue(cx);
+            compassLevelY.writeValue(cy);
+            compassLevelZ.writeValue(cz);
+            compassHeading.writeValue(compassHeadingValue);
+        }
+    } else {
+        Serial.print("Central disconnected: ");
+        Serial.println(" - ");
+        digitalWrite(LED_BUILTIN, LOW);
+    }
+
+    //Now lets get the history set up.
+    BLE_StartTimeH = BLE_StartTime;
 
 }
 
 
 
 void audioCallBack(){
-// query the number of bytes available
-int bytesAvailable = PDM.available();
+    // query the number of bytes available
+    int bytesAvailable = PDM.available();
 
 
 
-// read into the sample buffer
-PDM.read(sampleBuffer, bytesAvailable);
+    // read into the sample buffer
+    PDM.read(sampleBuffer, bytesAvailable);
 
 
 
-// 16-bit, 2 bytes per sample
-samplesRead = bytesAvailable / 2;
+    // 16-bit, 2 bytes per sample
+    samplesRead = bytesAvailable / 2;
 }
