707d0abe60406bee23ab688356c94e9c69326956
Memeapie <samuel47@virginmedia.com>
2021-04-07 16:38:10 +0100

    Cleaned Library Imports

--- main_project/main_project.ino
+++ main_project/main_project.ino
@@ -4,31 +4,38 @@
 #include <Arduino_LSM9DS1.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  while (!Serial);
+    // Initialise Serial Output
+    Serial.begin(9600);
+    while (!Serial);
 
-  PDM.onReceive(onPDMdata);
+    // Initialise Temp/Humidity Sensor
+    if (!HTS.begin()) {
+        Serial.println("Failed to initialize humidity temperature sensor!");
+        while (1);
+    }
 
-  if (!HTS.begin()) {
-    Serial.println("Failed to initialize humidity temperature sensor!");
-    while (1);
-  }
+    // Initialise PDM
+    if (!PDM.begin(1, 16000)) {
+        Serial.println("Failed to start PDM!");
+        while (1);
+    }
 
-  if (!PDM.begin(1, 16000)) {
-    Serial.println("Failed to start PDM!");
-    while (1);
-  }
+    // Initialise IMU
+    if (!IMU.begin()) {
+        Serial.println("Failed to initialize IMU!");
+        while (1);
+    }
 
-  if (!IMU.begin()) {
-    Serial.println("Failed to initialize IMU!");
-    while (1);
-  }
+    // Initialise Bluetooth Module
+    if (!BLE.begin()){
+        Serial.println("starting BLE failed!");
+        while (1);
+    }
 
 }
 
 void loop() {
 
-//this is a test
+
 
 }
