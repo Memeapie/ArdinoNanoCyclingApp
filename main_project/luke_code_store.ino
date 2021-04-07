164b2c233e35649ea1a2921e39a162b71c2bc81a
lw8256n <luke@threluja.co.uk>
2021-04-07 21:21:46 +0100

    Attempting to add elevationGain variable, not working as intended though unfortunately

--- main_project/luke_code_store.ino
+++ main_project/luke_code_store.ino
@@ -1,12 +1,16 @@
-//place for luke to store code to show progress, will move into main_project.ino when complete
-
-
 #include <Arduino_LPS22HB.h>
 #include <math.h> //library for maths
 
 
 boolean testing = true;
+int readAttempts = 0;
+boolean secondRead = false; //using second read at the minute because the first read is being weird
 double pressureAtSeaLevel = 1023.0; //atmpshpehric pressure at sea level, need to get a value for kent coast// currently using manston reading at 1800 on 07/04
+double firstHalfofElevationCalculation = 0;
+double elevation = 0;
+double elevationH = 0;
+double elevationGain = 0;
+
 //double pow;
 
 void setup() {
@@ -27,9 +31,24 @@ void updatePressure () {
  double pressureKPA = BARO.readPressure();
 
   if (testing) {
+    Serial.print("elevation History = "); //forTesting purposes
+    Serial.print(elevationH);
+    Serial.println(" m");
+    Serial.print("elevation Gain = "); //something weird happens, elevation gain automatically becomes like a value greater than 2800;
+    Serial.print(elevationGain);
+    Serial.println(" m");
+
+    Serial.print("readAttempts");
+    Serial.print(readAttempts);
+    Serial.println();
+    Serial.print("secondRead = ");
+    Serial.print(secondRead);
+    Serial.println();
+
     Serial.print("Pressure = ");
     Serial.print(pressureKPA);
     Serial.println(" kPa");
+
   }
 
   double pressureHPA = pressureKPA * 10;
@@ -39,8 +58,8 @@ void updatePressure () {
     Serial.println(" hPa");
   }
 
-  double firstHalfofElevationCalculation = pow((pressureHPA/pressureAtSeaLevel), (1/5.255));
-  double elevation = 44340 * (1-firstHalfofElevationCalculation);
+  firstHalfofElevationCalculation = pow((pressureHPA/pressureAtSeaLevel), (1/5.255));
+  elevation = 44340 * (1-firstHalfofElevationCalculation);
 
   if (testing) {
     Serial.print("firstHalfofElevationCalculation = ");
@@ -51,6 +70,35 @@ void updatePressure () {
     Serial.println(" m");
 
   }
+
+  if (!secondRead) { //doesn't really work, need to fix this
+    readAttempts++;
+    if (readAttempts=1) {
+      secondRead = true;
+    }
+  } else {
+    if (elevationH = 0 || elevationH > 2000) { //elevationH does something weird on first read therefore if we reach outlier, we reset history value
+      elevationH = elevation;
+    } else {
+      if (elevation != elevationH) {
+
+         elevationGain = elevationGain + (elevation - elevationH); //doesn't work as intended
+         elevationH = elevation;
+      }
+    }
+  }
+
+  if (testing) {
+    Serial.print("elevation History = ");
+    Serial.print(elevationH);
+    Serial.println(" m");
+
+    Serial.print("elevation Gain = ");
+    Serial.print(elevationGain);
+    Serial.println(" m");
+  }
+
+
 
    Serial.println();
 
